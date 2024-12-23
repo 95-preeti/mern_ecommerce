dcfdfc5049f5e3e7402583c65621d710951479d5
Preeti yadav <preetiyadav30430@gmail.com>
2024-12-23 19:24:13 +0530

    create a userSignin in backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB =require('./config/db')
 const router = require('./routes')
@@ -9,6 +10,7 @@ const app= express()
 app.use(cors())
 app.use(express.json())
 app.use("/api",router)
+app.use(cookieParser())
 
 const PORT =8080 || process.env.PORT
 
